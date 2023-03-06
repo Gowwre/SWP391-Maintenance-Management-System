7f2f0fb48963f4ce8300215c68352491a098ed79
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-03-06 19:21:39 +0700

    refactor: changed to namedParameterJdbcTemplate

--- MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/dao/FaultDevice/FaultedDeviceRepository.java
+++ MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/dao/FaultDevice/FaultedDeviceRepository.java
@@ -8,44 +8,51 @@ import com.fptu.maintenancemanagersystem.model.FaultedDevice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
- *
  * @author lmphi
  */
 @Repository
 public class FaultedDeviceRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     public List<FaultedDevice> getAll() {
         String SQL = "Select * from FaultedDevice";
         return jdbcTemplate.query(SQL, new BeanPropertyRowMapper<>(FaultedDevice.class));
     }
-    
+
     public FaultedDevice get() {
         return null;
     }
-    
-    public void updateAssignedStaff(int faultDeviceId, int assignedStaffId) {
-        String sql = "UPDATE FaultedDevice SET assign_staff_id = ? WHERE faulted_device_id = ?";
-        try {
-            jdbcTemplate.update(sql, assignedStaffId, faultDeviceId);
-        }catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
+    public void updateAssignedStaff(int assignStaffId, int faultedDeviceId) {
+        String sql = "UPDATE FaultedDevice SET assign_staff_id = :assignStaffId WHERE faulted_device_id = :faultedDeviceId";
+
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("assignStaffId", assignStaffId);
+        parameters.addValue("faultedDeviceId", faultedDeviceId);
+
+        namedParameterJdbcTemplate.update(sql, parameters);
+
+
+    }
 
 
     public void update() {
-        
+
     }
-    
+
     public void delete() {
-        
+
     }
 
 }
