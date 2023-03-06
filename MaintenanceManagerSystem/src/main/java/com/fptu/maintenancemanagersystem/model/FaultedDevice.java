33f841d5302a80318ee62bde93e8f2f62cec17dd
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-03-06 19:22:34 +0700

    refactor: changed model class name

--- MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/model/FaultedDevice.java
+++ MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/model/FaultedDevice.java
@@ -1,16 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.fptu.maintenancemanagersystem.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
+/**
+ *
+ * @author lmphi
+ */
 
 @Data
 @NoArgsConstructor
-public class FaultedDevice implements Serializable {
-    private int faultedDeviceId;
-    private int equipmentId;
-    private int issueId;
-    private int assignStaffId;
-
+public class FaultedDevice {
+   int faultDeviceId;
+   int roomId;
+   int equipmentId;
+   int assignedStaffId;
 }
