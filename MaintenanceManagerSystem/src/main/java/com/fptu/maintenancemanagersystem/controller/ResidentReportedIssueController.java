e8b35e18ae95b9aeba7410e7bdbd9a3ca25bff7d
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-03-07 18:06:45 +0700

    Update ResidentReportedIssueController.java

--- MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/controller/ResidentReportedIssueController.java
+++ MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/controller/ResidentReportedIssueController.java
@@ -39,7 +39,7 @@ public class ResidentReportedIssueController {
             return "managerPages/reportedIssueList";
         } catch (Exception e) {
             model.addAttribute("error", e.getMessage());
-            return "errorPage";
+            return "error";
         }
     }
     
@@ -52,7 +52,7 @@ public class ResidentReportedIssueController {
             return "reportForm/reportFormStep1";
         } catch (Exception e) {
            model.addAttribute("error", e.getMessage());
-           return "errorPage";
+           return "error";
         }
     }
 
