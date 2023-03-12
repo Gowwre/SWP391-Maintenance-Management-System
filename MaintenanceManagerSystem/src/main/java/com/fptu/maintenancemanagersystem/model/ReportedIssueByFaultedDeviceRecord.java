3d6838ad78e8a42c2f47ab51ffbf1f29044dc40c
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-03-12 22:17:29 +0700

    Update ReportedIssueByFaultedDeviceRecord.java

--- MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/model/ReportedIssueByFaultedDeviceRecord.java
+++ MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/model/ReportedIssueByFaultedDeviceRecord.java
@@ -3,20 +3,10 @@ package com.fptu.maintenancemanagersystem.model;
 import java.time.LocalDate;
 
 
-public record ReportedIssueByFaultedDeviceRecord(int faultedDeviceId, int issueId, String residentName, String residentPhone,
-                                                 String residentEmail, String description, LocalDate reportedDate, int roomId,
+public record ReportedIssueByFaultedDeviceRecord(int faultedDeviceId, int issueId, String residentName,
+                                                 String residentPhone,
+                                                 String residentEmail, String description, LocalDate reportedDate,
+                                                 int roomId,
                                                  String roomNumber) {
-    public ReportedIssueByFaultedDeviceRecord(int faultedDeviceId, int issueId, String residentName, String residentPhone, String residentEmail, String description, LocalDate reportedDate, int roomId, String roomNumber) {
-        this.faultedDeviceId = faultedDeviceId;
-        this.issueId = issueId;
-        this.residentName = residentName;
-        this.residentPhone = residentPhone;
-        this.residentEmail = residentEmail;
-        this.description = description;
-        this.reportedDate = reportedDate;
-        this.roomId = roomId;
-        this.roomNumber = roomNumber;
-
-    }
 }
 
